2580700ff928e2bceb68c88848530dae882c66d1
Lluciocc <114759545+Lluciocc@users.noreply.github.com>
2026-05-12 18:40:27 +0200

    Update time.c

--- src/userland/cli/time.c
+++ src/userland/cli/time.c
@@ -2,7 +2,8 @@
 // This software is released under the GNU General Public License v3.0. See LICENSE file for details.
 // This header needs to maintain in any file it is present in, as per the GPL license terms.
 
-#include "../libc/stdlib.h"
+#include "stdlib.h"
+#include "syscall.h"
 
 #define CMDLINE_MAX 512
 
